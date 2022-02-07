3101fc35c546813f846fb60fcd19ee337e16399b
AlessandroManente <manente.alessandro@gmail.com>
2022-02-07 18:56:03 +0100

    add till part 17 finished

--- Action RPG Tutorial/Player/Player.gd
+++ Action RPG Tutorial/Player/Player.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D 
 
-const ACCELERATION = 500
-const MAX_SPEED = 80
-const FRICTION = 500
+export(int) var ACCELERATION = 500
+export(int) var MAX_SPEED = 80
+export(int) var ROLL_SPEED = 120
+export(int) var FRICTION = 500
 
 enum{
 	MOVE,
@@ -12,21 +13,27 @@ enum{
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
+var stats = PlayerStats
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = $AnimationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
+onready var hurtbox = $Hurtbox
 
 func _ready():
+	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
-func _physiscs_process(delta):
+func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
 			
 		ROLL:
-			pass
+			roll_state()
 			
 		ATTACK:
 			attack_state()
@@ -39,22 +46,46 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = roll_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	velocity = move_and_slide(velocity)
+	move();
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
+func roll_state():
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
 func attack_state():
+	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+
+func move():
+	velocity = move_and_slide(velocity)
+
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = MOVE
 
 func attack_animation_finished():
 	state = MOVE
+	
+func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
+	hurtbox.start_invincibility(0.5)
+	hurtbox.create_hit_effect()
